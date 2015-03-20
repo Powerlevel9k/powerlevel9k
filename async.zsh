Powerlevel9k/powerlevel9k

commit 5c2a7da7c34f11670bb65c6d28be7636fa45f217

Simplify _async_job function.

async.zsh
@@ -10,23 +10,18 @@
 
 # Wrapper for jobs executed by the async worker, gives output in parseable format with execution time
 _async_job() {
-	local out
-	# store the job identifier
-	local job=$1
-	# reset the identifier
-	1=""
 	# store start time
 	local start=$EPOCHREALTIME
 
 	# run the command
-	out=$($job $@ 2>&1)
+	local out=$($@ 2>&1)
 	local ret=$?
 
 	# Grab mutex lock
 	read -ep >/dev/null
 
-	# return output
-	print -r -N -n -- $job $ret "$out" $(( $EPOCHREALTIME - $start ))$'\0'
+	# return output (<job_name> <return_code> <output> <duration>)
+	print -r -N -n -- $1 $ret "$out" $(( $EPOCHREALTIME - $start ))$'\0'
 
 	# Unlock mutex
 	print -p "t"

commit 6db6652b43d35f8fb0e92a2f3a4d9915d7f1b05b

Undo 5c2a7da, caused segfault on Ubuntu.

async.zsh
@@ -12,16 +12,18 @@
 _async_job() {
 	# store start time
 	local start=$EPOCHREALTIME
+	local job=$1
+	1=""
 
 	# run the command
-	local out=$($@ 2>&1)
+	local out=$($job $@ 2>&1)
 	local ret=$?
 
 	# Grab mutex lock
 	read -ep >/dev/null
 
 	# return output (<job_name> <return_code> <output> <duration>)
-	print -r -N -n -- $1 $ret "$out" $(( $EPOCHREALTIME - $start ))$'\0'
+	print -r -N -n -- $job $ret "$out" $(( $EPOCHREALTIME - $start ))$'\0'
 
 	# Unlock mutex
 	print -p "t"