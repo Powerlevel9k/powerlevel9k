337a00cf90a87ebfd5041ce9fb01839c3d5134f2
Dominik Ritter <dritter03@googlemail.com>
2017-01-09 20:06:08 +0100

    Add useful information about switching users in test-VM

--- test-vm/plain.sh
+++ test-vm/plain.sh
@@ -4,4 +4,14 @@ echo '
 print -P "%F{blue}INFO:%f Set your configuration in powerlevel9k.config in your powerlevel9k root folder for easier testing."
 source /vagrant_data/powerlevel9k.config &>/dev/null
 
+print -P "\nYou could switch to one of the following users (password is \"vagrant\")"
+print -P "su - vagrant-antibody"
+print -P "su - vagrant-antigen"
+print -P "su - vagrant-omz"
+print -P "su - vagrant-prezto"
+print -P "su - vagrant-prezto-community"
+print -P "su - vagrant-zgen"
+print -P "su - vagrant-zplug"
+print -P "su - vagrant-zplugin"
+
 source /vagrant_data/powerlevel9k.zsh-theme' > ~/.zshrc
