894fa6a6d12828c9369dadb02d8a7d266fde622a
Christo Kotze <onaforeignshore@hotmail.co.uk>
2018-05-04 14:20:19 +0400

    Updated incon functions, See details below
    functions/icons.zsh
    - changed core icon definitions to use registerIcon()
    
    segments/vcs.p9k
    - moved icon override for P9K_HIDE_BRANCH_ICON to this file instead of
    in icons.zsh
    
    powerlevel9k.zsh-theme
    - changed the sourcing order of icons and utilities so that defined() is
    available to registerIcon()

--- functions/icons.zsh
+++ functions/icons.zsh
@@ -15,9 +15,26 @@
 #   boxes or bizarre characters below, your fonts are not correctly installed. If
 #   you do not want to install a special font, you can set `P9K_MODE` to
 #   `compatible`. This shows all icons in regular symbols.
+#
+# For specific fonts configurations, please refer to:
+# - [Awesome-Patched Font](https://github.com/gabrielelana/awesome-terminal-fonts/tree/patching-strategy/patched)
+# - [fontconfig with awesome-font](https://github.com/gabrielelana/awesome-terminal-fonts)
+# - [mapped fontconfig with awesome-font](https://github.com/gabrielelana/awesome-terminal-fonts) and don't forget
+#   to source the font maps in your startup script
+# - [nerd-font patched (complete)](https://github.com/ryanoasis/nerd-fonts) and the [cheat sheet](http://nerdfonts.com/#cheat-sheet)
+# - [Powerline-Patched Font](https://github.com/Lokaltog/powerline-fonts)
 ##
 
+# Set the right locale to protect special characters
+local LC_ALL="" LC_CTYPE="en_US.UTF-8"
 typeset -gAH icons
+
+if [[ P9K_MODE == "awesome-mapped-fontconfig" && -z "$AWESOME_GLYPHS_LOADED" ]]; then
+    echo "Powerlevel9k warning: Awesome-Font mappings have not been loaded.
+    Source a font mapping in your shell config, per the Awesome-Font docs
+    (https://github.com/gabrielelana/awesome-terminal-fonts),
+    or use a different Powerlevel9k font configuration.";
+fi
 
 ################################################################
 # @description
@@ -43,7 +60,7 @@ typeset -gAH icons
 # @example
 #   registerIcon "LOCK_ICON"  $'\UE0A2'  $'\UE138'  $'\UF023'  '\u'$CODEPOINT_OF_AWESOME_LOCK  $'\UF023'
 ##
-registerIcon() {
+function registerIcon() {
   local map
 	local ICON_USER_VARIABLE=P9K_${1}
 	if defined "$ICON_USER_VARIABLE"; then # check for icon override first
@@ -60,100 +77,22 @@ registerIcon() {
 	icons[$1]=${map}
 }
 
-# Initialize the icon list according to the user's `P9K_MODE`.
-typeset -gAH icons
-case $P9K_MODE in
-  'flat'|'awesome-patched')
-    # Awesome-Patched Font required! See:
-    # https://github.com/gabrielelana/awesome-terminal-fonts/tree/patching-strategy/patched
-    # Set the right locale to protect special characters
-    local LC_ALL="" LC_CTYPE="en_US.UTF-8"
-    icons=(
-      LEFT_SEGMENT_SEPARATOR         $'\uE0B0'              # 
-      RIGHT_SEGMENT_SEPARATOR        $'\uE0B2'              # 
-      LEFT_SEGMENT_END_SEPARATOR     ' '                    # Whitespace
-      LEFT_SUBSEGMENT_SEPARATOR      $'\uE0B1'              # 
-      RIGHT_SUBSEGMENT_SEPARATOR     $'\uE0B3'              # 
-      MULTILINE_FIRST_PROMPT_PREFIX  $'\u256D'$'\U2500'     # ╭─
-      MULTILINE_NEWLINE_PROMPT_PREFIX  $'\u251C'$'\U2500'   # ├─
-      MULTILINE_LAST_PROMPT_PREFIX   $'\u2570'$'\U2500 '    # ╰─
-    )
-  ;;
-  'awesome-fontconfig')
-    # fontconfig with awesome-font required! See
-    # https://github.com/gabrielelana/awesome-terminal-fonts
-    # Set the right locale to protect special characters
-    local LC_ALL="" LC_CTYPE="en_US.UTF-8"
-    icons=(
-      LEFT_SEGMENT_SEPARATOR         $'\uE0B0'              # 
-      RIGHT_SEGMENT_SEPARATOR        $'\uE0B2'              # 
-      LEFT_SEGMENT_END_SEPARATOR     ' '                    # Whitespace
-      LEFT_SUBSEGMENT_SEPARATOR      $'\uE0B1'              # 
-      RIGHT_SUBSEGMENT_SEPARATOR     $'\uE0B3'              # 
-      MULTILINE_FIRST_PROMPT_PREFIX  $'\u256D'$'\U2500'     # ╭─
-      MULTILINE_NEWLINE_PROMPT_PREFIX  $'\u251C'$'\U2500'   # ├─
-      MULTILINE_LAST_PROMPT_PREFIX   $'\u2570'$'\U2500 '    # ╰─
-    )
-  ;;
-  'awesome-mapped-fontconfig')
-    # mapped fontconfig with awesome-font required! See
-    # https://github.com/gabrielelana/awesome-terminal-fonts
-    # don't forget to source the font maps in your startup script
-    # Set the right locale to protect special characters
-    local LC_ALL="" LC_CTYPE="en_US.UTF-8"
-
-    if [ -z "$AWESOME_GLYPHS_LOADED" ]; then
-        echo "Powerlevel9k warning: Awesome-Font mappings have not been loaded.
-        Source a font mapping in your shell config, per the Awesome-Font docs
-        (https://github.com/gabrielelana/awesome-terminal-fonts),
-        Or use a different Powerlevel9k font configuration.";
-    fi
-
-    icons=(
-      LEFT_SEGMENT_SEPARATOR         $'\uE0B0'              # 
-      RIGHT_SEGMENT_SEPARATOR        $'\uE0B2'              # 
-      LEFT_SEGMENT_END_SEPARATOR     ' '                    # Whitespace
-      LEFT_SUBSEGMENT_SEPARATOR      $'\uE0B1'              # 
-      RIGHT_SUBSEGMENT_SEPARATOR     $'\uE0B3'              # 
-      MULTILINE_FIRST_PROMPT_PREFIX  $'\u256D'$'\U2500'     # ╭─
-      MULTILINE_NEWLINE_PROMPT_PREFIX  $'\u251C'$'\U2500'   # ├─
-      MULTILINE_LAST_PROMPT_PREFIX   $'\u2570'$'\U2500 '    # ╰─
-    )
-  ;;
-  'nerdfont-complete'|'nerdfont-fontconfig')
-    # nerd-font patched (complete) font required! See
-    # https://github.com/ryanoasis/nerd-fonts
-    # http://nerdfonts.com/#cheat-sheet
-    # Set the right locale to protect special characters
-    local LC_ALL="" LC_CTYPE="en_US.UTF-8"
-    icons=(
-      LEFT_SEGMENT_SEPARATOR         $'\uE0B0'              # 
-      RIGHT_SEGMENT_SEPARATOR        $'\uE0B2'              # 
-      LEFT_SEGMENT_END_SEPARATOR     ' '                    # Whitespace
-      LEFT_SUBSEGMENT_SEPARATOR      $'\uE0B1'              # 
-      RIGHT_SUBSEGMENT_SEPARATOR     $'\uE0B3'              # 
-      MULTILINE_FIRST_PROMPT_PREFIX  $'\u256D'$'\U2500'     # ╭─
-      MULTILINE_NEWLINE_PROMPT_PREFIX  $'\u251C'$'\U2500'   # ├─
-      MULTILINE_LAST_PROMPT_PREFIX   $'\u2570'$'\U2500 '    # ╰─
-    )
-  ;;
-  *)
-    # Powerline-Patched Font required!
-    # See https://github.com/Lokaltog/powerline-fonts
-    # Set the right locale to protect special characters
-    local LC_ALL="" LC_CTYPE="en_US.UTF-8"
-    icons=(
-      LEFT_SEGMENT_SEPARATOR         $'\uE0B0'              # 
-      RIGHT_SEGMENT_SEPARATOR        $'\uE0B2'              # 
-      LEFT_SEGMENT_END_SEPARATOR     ' '                    # Whitespace
-      LEFT_SUBSEGMENT_SEPARATOR      $'\uE0B1'              # 
-      RIGHT_SUBSEGMENT_SEPARATOR     $'\uE0B3'              # 
-      MULTILINE_FIRST_PROMPT_PREFIX  $'\u256D'$'\U2500'     # ╭─
-      MULTILINE_NEWLINE_PROMPT_PREFIX  $'\u251C'$'\U2500'   # ├─
-      MULTILINE_LAST_PROMPT_PREFIX   $'\u2570'$'\U2500 '    # ╰─
-    )
-  ;;
-esac
+#                                                                                                                           
+registerIcon "LEFT_SEGMENT_SEPARATOR"           $'\uE0B0'           $'\uE0B0'           $'\uE0B0'           $'\uE0B0'           $'\uE0B0'
+#                                                                                                                           
+registerIcon "RIGHT_SEGMENT_SEPARATOR"          $'\uE0B2'           $'\uE0B2'           $'\uE0B2'           $'\uE0B2'           $'\uE0B2'
+#                                               Whitespace          Whitespace          Whitespace          Whitespace          Whitespace
+registerIcon "LEFT_SEGMENT_END_SEPARATOR"       ' '                 ' '                 ' '                 ' '                 ' '
+#                                                                                                                           
+registerIcon "LEFT_SUBSEGMENT_SEPARATOR"        $'\uE0B1'           $'\uE0B1'           $'\uE0B1'           $'\uE0B1'           $'\uE0B1'
+#                                                                                                                           
+registerIcon "RIGHT_SUBSEGMENT_SEPARATOR"       $'\uE0B3'           $'\uE0B3'           $'\uE0B3'           $'\uE0B3'           $'\uE0B3'
+#                                               ╭─                  ╭─                 ╭─                  ╭─                 ╭─
+registerIcon "MULTILINE_FIRST_PROMPT_PREFIX"    $'\u256D'$'\U2500'  $'\u256D'$'\U2500'  $'\u256D'$'\U2500'  $'\u256D'$'\U2500'  $'\u256D'$'\U2500'
+#                                               ├─                 ├─                  ├─                 ├─                  ├─
+registerIcon "MULTILINE_NEWLINE_PROMPT_PREFIX"  $'\u251C'$'\U2500'  $'\u251C'$'\U2500'  $'\u251C'$'\U2500'  $'\u251C'$'\U2500'  $'\u251C'$'\U2500'
+#                                               ╰─                 ╰─                  ╰─                 ╰─                  ╰─
+registerIcon "MULTILINE_LAST_PROMPT_PREFIX"     $'\u2570'$'\U2500 ' $'\u2570'$'\U2500 ' $'\u2570'$'\U2500 ' $'\u2570'$'\U2500 ' $'\u2570'$'\U2500 '
 
 # Override the above icon settings with any user-defined variables.
 case $P9K_MODE in
@@ -172,9 +111,6 @@ case $P9K_MODE in
 		icons[RIGHT_SEGMENT_SEPARATOR]=$'\u2B82'                # ⮂
 	;;
 esac
-
-# Hide branch icon if user wants it hidden
-[[ "$P9K_HIDE_BRANCH_ICON" == true ]] && icons[VCS_BRANCH_ICON]=''
 
 ################################################################
 # @description
